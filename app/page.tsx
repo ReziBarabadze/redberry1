36d0cb227579ebce3993dfae3296496f4f8f15b7
Rezi <rezibarabadze153@gmail.com>
2025-03-12 19:55:04 +0400

    install Next.js, MaterialUI & create  header component

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const page = () => {
+  return <Box sx={{ paddingTop: "100px" }}>page</Box>;
+};
+
+export default page;
---
+++ components/Header.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <Box
+      sx={{
+        height: "100px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingInline: "120px",
+        position: "fixed",
+        top: 0,
+        width: "100%",
+        boxSizing: "border-box",
+      }}
+    >
+      <Box sx={{ display: "flex", cursor: "pointer" }}>
+        <Typography
+          sx={{
+            fontFamily: "Fredoka One",
+            fontWeight: 400,
+            fontSize: "31px",
+            lineHeight: "100%",
+            color: "#8338EC",
+            marginRight: "4px",
+          }}
+        >
+          Momentum
+        </Typography>
+        <Image src="images/Hourglass.svg" alt="icon" width={38} height={38} />
+      </Box>
+      <Box sx={{ display: "flex", gap: "40px" }}>
+        <Button
+          sx={{
+            fontFamily: "FiraGO",
+            fontWeight: 400,
+            fontSize: "16px",
+            lineHeight: "100%",
+            border: "1px solid #8338EC",
+            color: "#212529",
+            width: "225px",
+            height: "40px",
+            borderRadius: "5px",
+          }}
+        >
+          თანამშრომლის შექმნა
+        </Button>
+        <Button
+          sx={{
+            fontFamily: "FiraGO",
+            fontWeight: 400,
+            fontSize: "16px",
+            lineHeight: "100%",
+            color: "#FFFFFF",
+            backgroundColor: "#8338EC",
+            width: "268px",
+            height: "40px",
+          }}
+        >
+          <span style={{ fontSize: "20px", marginRight: "4px" }}>+</span>{" "}
+          შექმენი ახალი დავალება
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
